55f9c72d1740d20060e62f3bdebdba2885229b89
cpilko <cpilko@users.noreply.github.com>
2024-07-17 02:17:04 +0000

    deploy: e0a43663f9613cb970c993d203e2a8d842bb6b75

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 const { parse } = require("csv-parse/sync");
+const yaml = require("js-yaml");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/base.css");
     eleventyConfig.addPassthroughCopy("src/favicon.ico");
     eleventyConfig.addPassthroughCopy("src/img/");
+    eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
     eleventyConfig.addDataExtension("csv", (contents) => {
       const records = parse(contents, {
       columns: true,
